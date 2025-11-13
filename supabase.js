fca644bb7b166acaa9b1dc70529e068f6a2219f4
Adetutu Oluwatobi Remilekun <109359916+Quantumfixithub@users.noreply.github.com>
2025-11-13 18:14:40 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const SUPABASE_URL = "https://amrvwccagixmktagoecv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
